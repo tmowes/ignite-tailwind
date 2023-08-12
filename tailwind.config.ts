bf59324b7ffb8fa925c493f67ef3f865de6c3238
tmowes <t-mowes@hotmail.com>
2023-08-12 17:58:40 -0300

    layout structure

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,12 @@ const config: Config = {
   content: ['./src/**/*.tsx'],
   theme: {
     extend: {
+      gridTemplateColumns: {
+        app: 'minmax(18rem, 20rem) 1fr',
+      },
+      maxWidth: {
+        app: '700px',
+      },
       colors: {
         julius: '#ff6900',
       },
